58104417f631156e094b4e3a37bd21fa5b363236
Dhruv Weaver <dhruv.weaver@gmail.com>
2021-11-06 13:33:27 -0400

    Added "working" map

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Map extends StatelessWidget {
   const Map({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Empty map');
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 13.0,
+      ),
+      layers: [
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(51.5, -0.09),
+              builder: (ctx) => Container(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
+      children: <Widget>[
+        TileLayerWidget(
+            options: TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'])),
+        MarkerLayerWidget(
+            options: MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(51.5, -0.09),
+              builder: (ctx) => Container(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
+        )),
+      ],
+    );
   }
 }
